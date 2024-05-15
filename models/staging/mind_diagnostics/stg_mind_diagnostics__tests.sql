840ac4ecde87dfb8a72b03889014f1d1cbecdcb6
David Reynolds <dgazeyreyn@gmail.com>
2024-05-15 17:19:38 +0000

    adding visitors_id field to stg_mind_diagnostics__visitors model

--- models/staging/mind_diagnostics/stg_mind_diagnostics__tests.sql
+++ models/staging/mind_diagnostics/stg_mind_diagnostics__tests.sql
@@ -1,7 +1,7 @@
 with
     source as (select * from {{ source("mind_diagnostics", "log_tests") }}),
 
-    tests as (select ip_address as tests_ip from source)
+    tests as (select id as tests_id, ip_address as tests_ip from source)
 
 select *
 from tests
