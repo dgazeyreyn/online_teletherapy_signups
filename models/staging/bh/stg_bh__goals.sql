2ae661049005fea5058c76650c3787df2407ca13
David Reynolds <davidgazeyreynolds@gmail.com>
2025-12-22 18:30:17 +0000

    incorporated testing into goals staging models and resolved timestamp inconsistencies

--- models/staging/bh/stg_bh__goals.sql
+++ models/staging/bh/stg_bh__goals.sql
@@ -5,8 +5,29 @@ with
 
         select
 
-            stat_affiliate_info5 as goals_mduid,
-            *
+            {{
+                dbt_utils.generate_surrogate_key(
+                    [
+                        "stat_datetime",
+                        "goal_name",
+                        "stat_ad_id",
+                        "stat_affiliate_info5",
+                    ]
+                )
+            }} as goal_id,
+            stat_datetime as conversion_at_est,
+            timestamp(
+                datetime(stat_datetime), 'America/New_York'
+            ) as conversion_at_utc,
+            goal_name,
+            country_name,
+            stat_ad_id,
+            stat_affiliate_info1,
+            stat_affiliate_info2,
+            stat_affiliate_info3,
+            stat_affiliate_info4,
+            stat_affiliate_info5 as mduid,
+            conversionsmobile_affiliate_click_id
 
         from source
 
