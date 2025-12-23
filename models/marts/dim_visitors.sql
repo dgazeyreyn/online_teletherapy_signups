1570139e5336c15d2dfa9f654c876dfe18f87f32
David Reynolds <davidgazeyreynolds@gmail.com>
2025-12-23 02:26:16 +0000

    created dimensional model and testing for visitors

---
+++ models/marts/dim_visitors.sql
@@ -0,0 +1,22 @@
+with visitors as (select * from {{ ref("int_visitors_one_2_one_relationship") }})
+
+select
+    {{ dbt_utils.generate_surrogate_key(["mduid"]) }} as visitor_key,
+
+    -- natural key
+    mduid,
+
+    -- descriptive attributes
+    ip_address,
+    browser,
+    os_name,
+    country_name,
+    region,
+    gclid_flag,
+
+    -- lifecycle timestamps
+    min(created_at) as first_seen_at_utc,
+    max(created_at) as last_seen_at_utc
+
+from visitors
+group by mduid, ip_address, browser, os_name, country_name, region, gclid_flag
