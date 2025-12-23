7f7b4c2bc1e2ce0cea59fcb7731b2805590dd9ee
David Reynolds <davidgazeyreynolds@gmail.com>
2025-12-23 00:57:39 +0000

    ensured a one-2-one relationship between ip address and mduid

---
+++ models/intermediate/int_visitors_one_2_one_relationship.sql
@@ -0,0 +1,20 @@
+with
+    visitors_raw as (select * from {{ ref("stg_md__visitors") }}),
+
+    -- Identify IPs with exactly one mduid
+    one_2_one_ips as (
+        select visitor_ip_address
+        from visitors_raw
+        group by visitor_ip_address
+        having count(distinct mduid) = 1
+    ),
+
+    deduped as (
+
+        select v.*
+        from visitors_raw v
+        join one_2_one_ips o on v.visitor_ip_address = o.visitor_ip_address
+    )
+
+select *
+from deduped
