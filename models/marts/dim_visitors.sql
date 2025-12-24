97db7db37521bf3c6c152f184c645939f80a6cb1
David Reynolds <davidgazeyreynolds@gmail.com>
2025-12-24 02:43:10 +0000

    modified cte to abide by dbt best practices

--- models/marts/dim_visitors.sql
+++ models/marts/dim_visitors.sql
@@ -1,22 +1,29 @@
-with visitors as (select * from {{ ref("int_visitors_resolved") }})
+with
+    visitors as (select * from {{ ref("int_visitors_resolved") }})
 
-select
-    {{ dbt_utils.generate_surrogate_key(["mduid"]) }} as visitor_key,
+    final as (
 
-    -- natural key
-    mduid,
+        select
+            {{ dbt_utils.generate_surrogate_key(["mduid"]) }} as visitor_key,
 
-    -- descriptive attributes
-    ip_address,
-    browser,
-    os_name,
-    country_name,
-    region,
-    gclid_flag,
+            -- natural key
+            mduid,
 
-    -- lifecycle timestamps
-    min(created_at) as first_seen_at_utc,
-    max(created_at) as last_seen_at_utc
+            -- descriptive attributes
+            ip_address,
+            browser,
+            os_name,
+            country_name,
+            region,
+            gclid_flag,
 
-from visitors
-group by mduid, ip_address, browser, os_name, country_name, region, gclid_flag
+            -- lifecycle timestamps
+            min(created_at) as first_seen_at_utc,
+            max(created_at) as last_seen_at_utc
+
+        from visitors
+        group by mduid, ip_address, browser, os_name, country_name, region, gclid_flag
+    )
+
+select *
+from final
