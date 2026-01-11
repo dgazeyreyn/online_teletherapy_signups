f05565d80917adad4bc9690964ba66d819eb3e82
David G Reynolds <davidgazeyreynolds@yahoo.com>
2026-01-11 16:07:02 -0500

    Add code for bucketing sparse category values

--- analysis/extract_features.py
+++ analysis/extract_features.py
@@ -1,6 +1,7 @@
 from google.cloud import bigquery
 import pandas as pd
 from pathlib import Path
+from sklearn.preprocessing import OneHotEncoder
 
 # ----------------------------
 # Config
@@ -59,3 +60,51 @@ if __name__ == "__main__":
 
     print(f"✅ Extracted {len(df):,} rows")
     print(f"📦 Saved to {output_path}")
+
+# ----------------------------
+# Encode features
+# ----------------------------
+
+# Create a new column for the bucketed categories
+
+# Compute counts
+os_counts = df['os_name'].value_counts()
+
+# Threshold for "Other"
+threshold = 100
+
+# Bucket rare categories into 'Other'
+df['os_name_bucketed'] = df['os_name'].apply(
+    lambda x: x if os_counts[x] >= threshold else 'Other'
+)
+
+
+
+
+
+
+categorical_cols = [
+    "browser",
+    "os_name",
+    "region",
+    "test_engagement_state",
+    "tests_taken_count_bucket",
+    "tests_completed_count_bucket",
+    "test_latency_bucket",
+    "first_test_taken",
+    "first_completed_test_taken",
+]
+
+X = df[categorical_cols]
+y = df["has_signup"]
+
+encoder = OneHotEncoder(
+    drop="first",          # reference category
+    sparse=False,
+    handle_unknown="ignore"
+)
+
+X_encoded = encoder.fit_transform(X)
+
+feature_names = encoder.get_feature_names_out(categorical_cols)
+X_encoded = pd.DataFrame(X_encoded, columns=feature_names)
